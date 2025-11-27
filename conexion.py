4481699f1e9de571d9a13e1639cfd1c4d827544e
ErickCJS <julonerick1@gmail.com>
2025-11-27 18:37:33 -0500

    confi
    confi

--- conexion.py
+++ conexion.py
@@ -1,16 +1,16 @@
-# import pymysql
+import pymysql
 
-# def conectarbd():
-#     try:
-#         return pymysql.connect(
-#             host='preguntas-calculadoramateriales.e.aivencloud.com',
-#             user='avnadmin',
-#             password='AVNS_LFRg88CLhVu59pB2C0e',
-#             db='cuestionario',
-#             port=10416,
-#             cursorclass=pymysql.cursors.DictCursor
-#         )
+def conectarbd():
+    try:
+        return pymysql.connect(
+            host='preguntas-calculadoramateriales.e.aivencloud.com',
+            user='avnadmin',
+            password='AVNS_LFRg88CLhVu59pB2C0e',
+            db='cuestionario',
+            port=10416,
+            cursorclass=pymysql.cursors.DictCursor
+        )
     
-#     except pymysql.Error as error:
-#         print(error)
-#         return False
+    except pymysql.Error as error:
+        print(error)
+        return False
